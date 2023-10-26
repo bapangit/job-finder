23a298eaf5503d1f35c8adf12aac55f9000dcd60
CHANDAN MANDAL <cmandal97531@gmail.com>
2023-10-26 16:31:31 +0530

    utils changes

--- src/utils/LocalStorageMethods.js
+++ src/utils/LocalStorageMethods.js
@@ -1,5 +1,4 @@
 const USER_ID_KEY = "100";
-const USER_TOKEN_KEY = "101";
 
 export const setUserIdInApp = (id) => {
   localStorage.setItem(USER_ID_KEY, id);
@@ -8,5 +7,6 @@ export const setUserIdInApp = (id) => {
 export const getUserId = () => {
   var id =
     localStorage.getItem(USER_ID_KEY) || parseInt(Math.random() * 100000);
+  setUserIdInApp(id);
   return id;
 };
